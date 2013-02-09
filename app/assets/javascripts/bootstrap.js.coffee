33cb54c1cd01bfd291adf1a487631a4e20ac819b
nktb40 <nktb40@gmail.com>
2013-02-10 03:17:45 +0400

    initial commit

---
+++ app/assets/javascripts/bootstrap.js.coffee
@@ -0,0 +1,8 @@
+jQuery ->
+  $(".new-note-input").popover({
+  animation: true,
+  placement: 'bottom',
+  html: true, 
+  trigger: 'click'})
+  $(".tooltip").tooltip()
+  $("a[rel=tooltip]").tooltip()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllMyNotes::Application.config.secret_token = '4d53db09e38c8957e5302837623b39662d37be40955bf1d3632edc0c591a956042246be6af2792f357a5131194b4624dc364250630defebde05733f4f14f5fd0'
