b6cec472c6f4c39c6bb0a4c9f201bd34095f2dd3
nktb40 <nktb40@gmail.com>
2013-02-14 17:16:24 +0400

    test deploy

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/nktb40/apps/allmynotes/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+AS_USER=nktb40
 set -u
 
 OLD_PIN="$PID.oldbin"
