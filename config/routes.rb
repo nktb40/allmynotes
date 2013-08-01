5eb2458bdc02ed2a3ba257edb3b2312abb4a1214
nktb40 <nktb40@gmail.com>
2013-08-01 12:26:45 +0400

    routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 AllMyNotes::Application.routes.draw do
+  root :to => 'notes#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
@@ -69,7 +70,7 @@ AllMyNotes::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'notes#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
